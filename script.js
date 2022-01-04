5e31c953cdfb9bde1c82a2ad669fd7c706a99eaf
rnl <rafael.nogueira@matera.com>
2022-01-04 16:00:40 -0300

    primeira aula

---
+++ script.js
@@ -0,0 +1,15 @@
+const form = document.querySelector("#form-notes");
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  //recuperar a nota digitada pelo usuário
+
+  //criar um article com esta nota
+
+  //adicionar o article na section
+
+  //atualizar o localStorage
+}
+
+form.addEventListener("submit", handleSubmit);
